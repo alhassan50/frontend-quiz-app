86fffa43e9e5a657933405e0e1267b226a4abd3c
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-06-12 08:40:29 +0000

    Display confetti on resluts page
    Confetti is displayed on the results page if the user has a score of
    atleast five(5)

--- src/components/quiz/Results.tsx
+++ src/components/quiz/Results.tsx
@@ -4,6 +4,8 @@ import Grid from "../../components/shared/Grid";
 import { useSelector } from "react-redux";
 import { getSelectedSubject } from "../../slices/subjectSlice";
 import Logo from "../shared/navbar/Logo";
+import Confetti from "react-confetti";
+import { useEffect, useState } from "react";
 
 // Styled-components
 const Section = styled.section`
@@ -69,35 +71,68 @@ const ResultContainer = styled.div`
 `;
 
 export default function Results({ quizScore, quizLength }: { quizScore: number, quizLength: number }) {
+    const [windowSize, setWindowSize] = useState<{width: number | undefined}>({
+        width: window.innerWidth - 20,
+    })
+
   const navigate = useNavigate();
-  const selectedCategory = useSelector(getSelectedSubject);
+  const selectedSubject = useSelector(getSelectedSubject);
+
+  useEffect(()=>{
+    const handleWindowResize = () => {
+        setWindowSize(prevDimensions => {
+            return {
+                ...prevDimensions,
+                width: window.innerWidth - 20,
+            }
+        })
+    }
+
+    window.addEventListener('resize', () => {
+        handleWindowResize()
+    })
+
+    return () => {
+        window.removeEventListener('resize', () => {
+            handleWindowResize()
+        })
+    };
+  }, [])
 
   const playAgain = () => {
     navigate('/');
   };
 
   return (
-    <Grid>
-      <Section>
-        <TextContainer>
-          <h1>
-            <span>Quiz completed </span>
-            <br />
-            You scored...
-          </h1>
-        </TextContainer>
-      </Section>
+    <>
+        {
+            quizScore >= 5 &&
+            <Confetti
+                width={windowSize.width}
+            />
+        }
+        <Grid>
+        <Section>
+            <TextContainer>
+            <h1>
+                <span>Quiz completed </span>
+                <br />
+                You scored...
+            </h1>
+            </TextContainer>
+        </Section>
 
-      <Section>
-        <ResultContainer>
-          {selectedCategory && <Logo selectedSubject={selectedCategory} />}
-          <h1>{quizScore}</h1>
-          <p>out of {quizLength}</p>
-        </ResultContainer>
-        <button type="button" className="btn-primary" onClick={playAgain}>
-          Play Again
-        </button>
-      </Section>
-    </Grid>
+        <Section>
+            <ResultContainer>
+            {selectedSubject && <Logo selectedSubject={selectedSubject} />}
+            <h1>{quizScore}</h1>
+            <p>out of {quizLength}</p>
+            </ResultContainer>
+            <button type="button" className="btn-primary" onClick={playAgain}>
+            Play Again
+            </button>
+        </Section>
+        </Grid>
+    </>
   );
 }
